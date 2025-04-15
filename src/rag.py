3ebdc4a064c6571e654a9972effedb7700b1d706
STetsing <41009393+STetsing@users.noreply.github.com>
2025-04-15 11:10:02 +0200

    changelog + thresholded

--- src/rag.py
+++ src/rag.py
@@ -113,13 +113,12 @@ class IntegratedRAGSystem:
 
         # check if any filename ends with .md and prind index and similarity
         for i in range(len(filenames)):
-            print(f"Filename: {filenames[i]}, Index: {top100_indices[i]}, Similarity: {similarities[top100_indices[i]]}")
             if filenames[i].endswith('CHANGELOG.md'):
                 #print(f"############## --------->Filename: {filenames[i]}, Index: {top100_indices[i]}, Similarity: {similarities[top100_indices[i]]}")
                 thr_top_indices.append(top100_indices[i])           
         
-        print(f"Thresholded top indices: {thr_top_indices}")
-        print(f"Similarities thresholded: {similarities[thr_top_indices]}")
+        for i in thr_top_indices:
+            print (f"Filename: {self.knowledge_base[i]['source']}, Index: {i}, Similarity: {similarities[i]}")
         return [self.knowledge_base[i] for i in thr_top_indices]
     
     def generate_response(self, query: str, retrieved_docs: List[Dict[str, Any]]) -> str:
